385c244f5d2464fa719ea9d5932aa82712537338
onevcat <onevcat@gmail.com>
2015-04-06 23:31:50 +0900

    Demo and gitignore

---
+++ Kingfisher-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kingfisher-Demo
+//
+//  Created by Wei Wang on 15/4/6.
+//  Copyright (c) 2015年 Wei Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
