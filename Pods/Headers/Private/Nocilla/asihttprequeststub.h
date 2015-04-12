6d4413ecd9afe581f748bba514643008a662700b
onevcat <onevcat@gmail.com>
2015-04-12 14:38:53 +0900

    Update nocilla

--- Pods/Headers/Private/Nocilla/asihttprequeststub.h
+++ Pods/Headers/Private/Nocilla/asihttprequeststub.h
@@ -1,6 +1,1 @@
-@interface ASIHTTPRequestStub : NSObject
-- (NSInteger)stub_responseStatusCode;
-- (NSData *)stub_responseData;
-- (NSDictionary *)stub_responseHeaders;
-- (void)stub_startRequest;
-@end
+../../../Nocilla/Nocilla/Hooks/ASIHTTPRequest/ASIHTTPRequestStub.h
