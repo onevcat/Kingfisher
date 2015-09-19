9edd8be9a13270739ea4aa56d09285a921ba9aa3
onevcat <onevcat@gmail.com>
2015-09-19 20:25:05 +0900

    Use HEX instead

--- Kingfisher/ImageCache.swift
+++ Kingfisher/ImageCache.swift
@@ -647,9 +647,9 @@ extension UIImage {
     }
 }
 
-private let pngHeader: [UInt8] = [137, 80, 78, 71, 13, 10, 26, 10]
-private let jpgHeaderSOI: [UInt8] = [0xff, 0xd8]
-private let jpgHeaderIF: [UInt8] = [0xff, 0xe0]
+private let pngHeader: [UInt8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
+private let jpgHeaderSOI: [UInt8] = [0xFF, 0xD8]
+private let jpgHeaderIF: [UInt8] = [0xFF, 0xE0]
 
 extension NSData {
     var kf_imageFormat: ImageFormat {
