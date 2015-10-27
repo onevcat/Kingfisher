6bb9d3791187ff2ce9e6e5d485ca8237bfadd47c
onevcat <onevcat@gmail.com>
2015-10-27 23:48:15 +0900

    Fix JEPG format issue
    Also include EXIF etc.

--- Kingfisher/UIImage+Extension.swift
+++ Kingfisher/UIImage+Extension.swift
@@ -30,7 +30,7 @@ import MobileCoreServices
 
 private let pngHeader: [UInt8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
 private let jpgHeaderSOI: [UInt8] = [0xFF, 0xD8]
-private let jpgHeaderIF: [UInt8] = [0xFF, 0xE0]
+private let jpgHeaderIF: [UInt8] = [0xFF]
 private let gifHeader: [UInt8] = [0x47, 0x49, 0x46]
 
 // MARK: - Image format
@@ -46,8 +46,7 @@ extension NSData {
             return .PNG
         } else if buffer[0] == jpgHeaderSOI[0] &&
             buffer[1] == jpgHeaderSOI[1] &&
-            buffer[2] == jpgHeaderIF[0] &&
-            buffer[3] == buffer[3] & jpgHeaderIF[1]
+            buffer[2] == jpgHeaderIF[0]
         {
             return .JPEG
         }else if buffer[0] == gifHeader[0] &&
