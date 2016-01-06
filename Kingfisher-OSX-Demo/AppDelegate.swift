9dd79109b52977bcd46dc0262c521f187715c004
onevcat <onevcat@gmail.com>
2016-01-06 17:12:11 +0900

    Add demo app for OSX

---
+++ Kingfisher-OSX-Demo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Kingfisher-OSX-Demo
+//
+//  Created by WANG WEI on 2016/01/06.
+//  Copyright © 2016年 Wei Wang. All rights reserved.
+//
+
+import Cocoa
+import Kingfisher
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Kingfisher-OSX-Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kingfisher-OSX-Demo
+//
+//  Created by WANG WEI on 2016/01/06.
+//  Copyright © 2016年 Wei Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
