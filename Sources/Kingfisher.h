e20dd0c8a12056befc5b66419ec9859d52006df1
onevcat <onevcat@gmail.com>
2016-01-22 23:00:40 +0900

    Structure project for Swift package manager

--- Sources/Kingfisher.h
+++ Sources/Kingfisher.h
@@ -24,7 +24,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Kingfisher.
 FOUNDATION_EXPORT double KingfisherVersionNumber;
