bb281c4ba0b1c7e033b51eb2c0fb9fd47046393e
onevcat <onevcat@gmail.com>
2016-07-10 20:10:34 +0900

    Remove swift lint for OS X and tvOS

--- Demo/Kingfisher-Demo/ViewController.swift
+++ Demo/Kingfisher-Demo/ViewController.swift
@@ -58,12 +58,11 @@ extension ViewController {
     override func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
         
         // This will cancel all unfinished downloading task when the cell disappearing.
-        // swiftlint:disable force_cast
         (cell as! CollectionViewCell).cellImageView.kf_cancelDownloadTask()
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        // swiftlint:disable force_cast
+        
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionViewCell", forIndexPath: indexPath) as! CollectionViewCell
         
         cell.cellImageView.kf_showIndicatorWhenLoading = true
