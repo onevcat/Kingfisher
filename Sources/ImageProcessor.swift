edba04a4b007b3870970cae9325bb71674bb689e
onevcat <onevcat@gmail.com>
2016-08-31 13:28:42 +0900

    Add identifier to image processor

--- Sources/ImageProcessor.swift
+++ Sources/ImageProcessor.swift
@@ -34,6 +34,7 @@ public enum ImageProcessItem {
 }
 
 public protocol ImageProcessor {
+    var identifier: String { get }
     func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image?
 }
 
@@ -41,7 +42,9 @@ typealias ProcessorImp = ((ImageProcessItem, KingfisherOptionsInfo) -> Image?)
 
 public extension ImageProcessor {
     func append(another: ImageProcessor) -> ImageProcessor {
-        return GeneralProcessor { item, options in
+        let newIdentifier = identifier.appending("|>\(another.identifier)")
+        return GeneralProcessor(identifier: newIdentifier) {
+            item, options in
             if let image = self.process(item: item, options: options) {
                 return another.process(item: .image(image), options: options)
             } else {
@@ -52,6 +55,7 @@ public extension ImageProcessor {
 }
 
 fileprivate struct GeneralProcessor: ImageProcessor {
+    let identifier: String
     let p: ProcessorImp
     func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
         return p(item, options)
@@ -59,6 +63,7 @@ fileprivate struct GeneralProcessor: ImageProcessor {
 }
 
 public struct DefaultProcessor: ImageProcessor {
+    public let identifier = ""
     public init() {}
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
         switch item {
@@ -71,13 +76,18 @@ public struct DefaultProcessor: ImageProcessor {
 }
 
 public struct RoundCornerImageProcessor: ImageProcessor {
-    
+    public let identifier: String
     public let cornerRadius: CGFloat
     public let targetSize: CGSize?
     
     public init(cornerRadius: CGFloat, targetSize: CGSize? = nil) {
         self.cornerRadius = cornerRadius
         self.targetSize = targetSize
+        if let size = targetSize {
+            self.identifier = "com.onevcat.Kingfisher.RoundCornerImageProcessor(\(cornerRadius)_\(size))"
+        } else {
+            self.identifier = "com.onevcat.Kingfisher.RoundCornerImageProcessor(\(cornerRadius))"
+        }
     }
     
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
@@ -92,10 +102,14 @@ public struct RoundCornerImageProcessor: ImageProcessor {
 }
 
 public struct ResizingImageProcessor: ImageProcessor {
+    public let identifier: String
     public let targetSize: CGSize
+    
     public init(targetSize: CGSize) {
         self.targetSize = targetSize
+        self.identifier = "com.onevcat.Kingfisher.ResizingImageProcessor(\(targetSize))"
     }
+    
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
         switch item {
         case .image(let image):
@@ -107,10 +121,12 @@ public struct ResizingImageProcessor: ImageProcessor {
 }
 
 public struct BlurImageProcessor: ImageProcessor {
+    public let identifier: String
     public let blurRadius: CGFloat
 
     public init(blurRadius: CGFloat) {
         self.blurRadius = blurRadius
+        self.identifier = "com.onevcat.Kingfisher.BlurImageProcessor(\(blurRadius))"
     }
     
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
@@ -125,12 +141,14 @@ public struct BlurImageProcessor: ImageProcessor {
 }
 
 public struct OverlayImageProcessor: ImageProcessor {
+    public var identifier: String
     public let overlay: Color
     public let fraction: CGFloat
     
     public init(overlay: Color, fraction: CGFloat = 0.5) {
         self.overlay = overlay
         self.fraction = fraction
+        self.identifier = "com.onevcat.Kingfisher.OverlayImageProcessor(\(overlay.hex)_\(fraction))"
     }
     
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
@@ -144,9 +162,11 @@ public struct OverlayImageProcessor: ImageProcessor {
 }
 
 public struct TintImageProcessor: ImageProcessor {
+    public let identifier: String
     public let tint: Color
     public init(tint: Color) {
         self.tint = tint
+        self.identifier = "com.onevcat.Kingfisher.TintImageProcessor(\(tint.hex))"
     }
     
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
@@ -160,6 +180,7 @@ public struct TintImageProcessor: ImageProcessor {
 }
 
 public struct ColorControlsProcessor: ImageProcessor {
+    public let identifier: String
     public let brightness: CGFloat
     public let contrast: CGFloat
     public let saturation: CGFloat
@@ -170,6 +191,7 @@ public struct ColorControlsProcessor: ImageProcessor {
         self.contrast = contrast
         self.saturation = saturation
         self.inputEV = inputEV
+        self.identifier = "com.onevcat.Kingfisher.ColorControlsProcessor(\(brightness)_\(contrast)_\(saturation)_\(inputEV))"
     }
     
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
@@ -183,6 +205,7 @@ public struct ColorControlsProcessor: ImageProcessor {
 }
 
 public struct BlackWhiteProcessor: ImageProcessor {
+    public let identifier = "com.onevcat.Kingfisher.BlackWhiteProcessor"
     public init() {}
     public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
         return ColorControlsProcessor(brightness: 0.0, contrast: 1.0, saturation: 0.0, inputEV: 0.7)
@@ -193,4 +216,19 @@ public struct BlackWhiteProcessor: ImageProcessor {
 infix operator |>: AdditionPrecedence
 public func |>(left: ImageProcessor, right: ImageProcessor) -> ImageProcessor {
     return left.append(another: right)
+}
+
+fileprivate extension Color {
+    var hex: String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgba = Int(r*255) << 24 | Int(g*255) << 16 | Int(b*255) << 8 | Int(a*255)
+        
+        return String(format:"#%08x", rgba)
+    }
 }
