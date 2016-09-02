0af9630b0d217a95697731ff4fe9c825d4a80007
onevcat <onevcat@gmail.com>
2016-09-02 11:59:12 +0900

    Refine API

--- Sources/ImageDownloader.swift
+++ Sources/ImageDownloader.swift
@@ -108,6 +108,19 @@ struct NoModifier: ImageDownloadRequestModifier {
     }
 }
 
+public struct AnyModifier: ImageDownloadRequestModifier {
+    
+    let block: (URLRequest) -> URLRequest?
+    
+    public func modified(for request: URLRequest) -> URLRequest? {
+        return block(request)
+    }
+
+    public init(modify: @escaping (URLRequest) -> URLRequest? ) {
+        block = modify
+    }
+}
+
 /// Protocol of `ImageDownloader`.
 public protocol ImageDownloaderDelegate: class {
     /**
