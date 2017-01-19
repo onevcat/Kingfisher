686c2e12dd2e400ee4cb14c3cdab33866186a575
onevcat <onevcat@gmail.com>
2017-01-19 11:09:49 +0900

    Update copyright year

--- Sources/ThreadHelper.swift
+++ Sources/ThreadHelper.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Wei Wang on 15/10/9.
 //
-//  Copyright (c) 2016 Wei Wang <onevcat@gmail.com>
+//  Copyright (c) 2017 Wei Wang <onevcat@gmail.com>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
