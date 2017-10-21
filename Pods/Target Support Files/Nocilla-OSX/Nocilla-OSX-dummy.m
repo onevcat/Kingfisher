7d78795b516af68a6ebc4cc7bb6faea496009048
onevcat <onevcat@gmail.com>
2017-10-22 00:12:52 +0900

    Upgrade cocoapod settting

--- Pods/Target Support Files/Nocilla-OSX/Nocilla-OSX-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Nocilla_OSX : NSObject
-@end
-@implementation PodsDummy_Nocilla_OSX
-@end
---
+++ Pods/Target Support Files/Nocilla-macOS/Nocilla-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Nocilla_macOS : NSObject
+@end
+@implementation PodsDummy_Nocilla_macOS
+@end
