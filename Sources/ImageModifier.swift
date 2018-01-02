85e2c437a9fbcdbf67f1fc97ef728c6ee30e0384
onevcat <onevcat@gmail.com>
2018-01-02 10:27:08 +0900

    Happy new year

--- Sources/ImageModifier.swift
+++ Sources/ImageModifier.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Ethan Gill on 2017/11/28.
 //
-//  Copyright (c) 2017 Ethan Gill <ethan.gill@me.com>
+//  Copyright (c) 2018 Ethan Gill <ethan.gill@me.com>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
