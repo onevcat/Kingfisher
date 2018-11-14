fe9f1b540f8ba67431b182b40b9ba5ba85434ad0
onevcat <onevcat@gmail.com>
2018-11-15 02:43:23 +0900

    Fix watchOS build

--- Sources/Extensions/WKInterfaceImage+Kingfisher.swift
+++ Sources/Extensions/WKInterfaceImage+Kingfisher.swift
@@ -77,7 +77,7 @@ extension KingfisherClass where Base: WKInterfaceImage {
                 DispatchQueue.main.safeAsync {
                     guard resource.downloadURL == self.webURL else {
                         let error = KingfisherError.imageSettingError(
-                            reason: .notCurrentSource(result: result.value, error: result.error, resource: resource))
+                            reason: .notCurrentSource(result: result.value, error: result.error, source: .network(resource)))
                         completionHandler?(.failure(error))
                         return
                     }
