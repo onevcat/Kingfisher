0c8bb7930e8c8c1edc7a9321f749d37b81dbc2f0
Dominique Stranz <dstranz@greywizard.com>
2019-06-25 12:03:04 +0200

    Add supported platforms to SPM

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Kingfisher",
-    // platforms: [.iOS("10.0"), .macOS("10.12"), tvOS("10.0"), .watchOS("3.0")],
+    platforms: [.iOS("10.0"), .macOS("10.12"), .tvOS("10.0"), .watchOS("3.0")],
     products: [
         .library(name: "Kingfisher", targets: ["Kingfisher"])
     ],
