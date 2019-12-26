3d037986d5eac9083c1bf27cb7842a90eafc2b97
carefree <946715806@qq.com>
2019-12-26 11:03:46 +0800

    Add changes for Xcode 11 SPM

--- Sources/Extensions/NSButton+Kingfisher.swift
+++ Sources/Extensions/NSButton+Kingfisher.swift
@@ -24,6 +24,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#if canImport(AppKit) && !targetEnvironment(macCatalyst)
 
 import AppKit
 
@@ -323,3 +324,5 @@ extension KingfisherWrapper where Base: NSButton {
         set { }
     }
 }
+
+#endif
