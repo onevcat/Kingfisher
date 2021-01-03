36b9676a7b23ad44396bcf8dde5200cac97e8e24
onevcat <onevcat@gmail.com>
2021-01-03 10:34:31 +0900

    Remove unnecessary SPM targets

--- Package.swift
+++ Package.swift
@@ -5,26 +5,12 @@ let package = Package(
     name: "Kingfisher",
     platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
     products: [
-        .library(name: "Kingfisher", targets: ["Kingfisher"]),
-        .library(name: "KingfisherSwiftUI", targets: ["KingfisherSwiftUI"]),
-        
-        .library(name: "KingfisherDynamic", type: .dynamic, targets: ["Kingfisher"]),
-        .library(name: "KingfisherSwiftUIDynamic", type: .dynamic, targets: ["KingfisherSwiftUI"]),
-        
-        .library(name: "KingfisherStatic", type: .static, targets: ["Kingfisher"]),
-        .library(name: "KingfisherSwiftUIStatic", type: .static, targets: ["KingfisherSwiftUI"])
+        .library(name: "Kingfisher", targets: ["Kingfisher"])
     ],
     targets: [
         .target(
             name: "Kingfisher",
-            path: "Sources",
-            exclude: ["SwiftUI"]
-        ),
-        .target(
-            name: "KingfisherSwiftUI",
-            dependencies: ["Kingfisher"],
-            path: "Sources",
-            sources: ["SwiftUI"]
+            path: "Sources"
         )
     ]
 )
