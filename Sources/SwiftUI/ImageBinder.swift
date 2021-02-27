715567df8689e8d5d7af0997a5d4b1240f70edf7
onevcat <onevcat@gmail.com>
2021-02-27 20:14:14 +0900

    Use weak image ref for ImageBinder

--- Sources/SwiftUI/ImageBinder.swift
+++ Sources/SwiftUI/ImageBinder.swift
@@ -48,7 +48,7 @@ extension KFImage {
 
         var isLoaded: Binding<Bool>
 
-        var loadedImage: KFCrossPlatformImage? = nil
+        weak var loadedImage: KFCrossPlatformImage? = nil
 
         @available(*, deprecated, message: "The `options` version is deprecated And will be removed soon.")
         init(source: Source?, options: KingfisherOptionsInfo? = nil, isLoaded: Binding<Bool>) {
