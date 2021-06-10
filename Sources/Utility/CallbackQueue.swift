426e0506ff50962c37c9a1b0b65aa6d9beec5f1e
wogus3602 <wogus3602@naver.com>
2021-06-10 12:39:41 +0900

    Modified for convention

--- Sources/Utility/CallbackQueue.swift
+++ Sources/Utility/CallbackQueue.swift
@@ -71,7 +71,7 @@ extension DispatchQueue {
     // This method will dispatch the `block` to self.
     // If `self` is the main queue, and current thread is main thread, the block
     // will be invoked immediately instead of being dispatched.
-    func safeAsync(_ block: @escaping ()->()) {
+    func safeAsync(_ block: @escaping () -> Void) {
         if self === DispatchQueue.main && Thread.isMainThread {
             block()
         } else {
