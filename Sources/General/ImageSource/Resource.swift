170db84f19023cd1d637d5388d671d1e454ddaae
onevcat <onevcat@gmail.com>
2021-10-03 22:19:28 +0900

    Use a stable default cache key for local files

--- Sources/General/ImageSource/Resource.swift
+++ Sources/General/ImageSource/Resource.swift
@@ -45,7 +45,7 @@ extension Resource {
     /// `.network` is returned.
     public func convertToSource(overrideCacheKey: String? = nil) -> Source {
         return downloadURL.isFileURL ?
-            .provider(LocalFileImageDataProvider(fileURL: downloadURL, cacheKey: overrideCacheKey ?? cacheKey)) :
+            .provider(LocalFileImageDataProvider(fileURL: downloadURL, cacheKey: overrideCacheKey ?? downloadURL.localFileCacheKey)) :
             .network(ImageResource(downloadURL: downloadURL, cacheKey: overrideCacheKey ?? cacheKey))
     }
 }
