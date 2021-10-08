33e824a827e73e5c0dfe7bac2b62dd16bdf4bbd5
onevcat <onevcat@gmail.com>
2021-10-08 10:28:49 +0900

    Wrap KFImage to a ZStack
    to prevent multiple child view issue when shown in a lazy container

--- Demo/Demo/Kingfisher-Demo/SwiftUIViews/LazyVStackDemo.swift
+++ Demo/Demo/Kingfisher-Demo/SwiftUIViews/LazyVStackDemo.swift
@@ -25,14 +25,23 @@
 //  THE SOFTWARE.
 
 import SwiftUI
+import Kingfisher
 
 @available(iOS 14.0, *)
 struct LazyVStackDemo: View {
+    @State private var singleImage = false
+    
     var body: some View {
         ScrollView {
+            // Checking for #1839
+            Toggle("Single Image", isOn: $singleImage).padding()
             LazyVStack {
                 ForEach(1..<700) { i in
-                    ImageCell(index: i).frame(width: 300, height: 300)
+                    if singleImage {
+                        KFImage.url(ImageLoader.roseImage(index: 1))
+                    } else {
+                        ImageCell(index: i).frame(width: 300, height: 300)
+                    }
                 }
             }
         }.navigationBarTitle(Text("Lazy Stack"), displayMode: .inline)
