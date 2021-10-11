5ecd8df18f1750efaa57d1b9623090156da01ef3
onevcat <onevcat@gmail.com>
2021-10-11 22:41:19 +0900

    Upgrade version in SPM

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Kingfisher",
-    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
+    platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
     products: [
         .library(name: "Kingfisher", targets: ["Kingfisher"])
     ],
