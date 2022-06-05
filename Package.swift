3f49e3f06aec3c299eff86ae89c0206ae7c690b5
onevcat <onevcat@gmail.com>
2022-06-05 20:43:30 +0900

    Support build docc from SPM

--- Package.swift
+++ Package.swift
@@ -14,3 +14,10 @@ let package = Package(
         )
     ]
 )
+
+#if swift(>=5.6)
+  // Add the documentation compiler plugin if possible
+  package.dependencies.append(
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+  )
+#endif
