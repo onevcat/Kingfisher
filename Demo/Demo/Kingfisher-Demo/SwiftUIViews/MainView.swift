82a3d683b1d1108ceb87514fc0b868c169ec277d
onevcat <onevcat@gmail.com>
2022-09-21 22:58:17 +0900

    Add a regression test scene for the issue

--- Demo/Demo/Kingfisher-Demo/SwiftUIViews/MainView.swift
+++ Demo/Demo/Kingfisher-Demo/SwiftUIViews/MainView.swift
@@ -31,23 +31,32 @@ import Kingfisher
 struct MainView: View {
     var body: some View {
         List {
-            Button(
-                action: {
-                    KingfisherManager.shared.cache.clearMemoryCache()
-                    KingfisherManager.shared.cache.clearDiskCache()
-                },
-                label: {
-                    Text("Clear Cache").foregroundColor(.blue)
-                }
-            )
-            NavigationLink(destination: SingleViewDemo()) { Text("Basic Image") }
-            NavigationLink(destination: SizingAnimationDemo()) { Text("Sizing Toggle") }
-            NavigationLink(destination: ListDemo()) { Text("List") }
-            NavigationLink(destination: LazyVStackDemo()) { Text("Stack") }
-            NavigationLink(destination: GridDemo()) { Text("Grid") }
-            NavigationLink(destination: AnimatedImageDemo()) { Text("Animated Image") }
-            NavigationLink(destination: GeometryReaderDemo()) { Text("Geometry Reader") }
-            NavigationLink(destination: TransitionViewDemo()) { Text("Transition") }
+            Section {
+                Button(
+                    action: {
+                        KingfisherManager.shared.cache.clearMemoryCache()
+                        KingfisherManager.shared.cache.clearDiskCache()
+                    },
+                    label: {
+                        Text("Clear Cache").foregroundColor(.blue)
+                    }
+                )
+            }
+            
+            Section(header: Text("Demo")) {
+                NavigationLink(destination: SingleViewDemo()) { Text("Basic Image") }
+                NavigationLink(destination: SizingAnimationDemo()) { Text("Sizing Toggle") }
+                NavigationLink(destination: ListDemo()) { Text("List") }
+                NavigationLink(destination: LazyVStackDemo()) { Text("Stack") }
+                NavigationLink(destination: GridDemo()) { Text("Grid") }
+                NavigationLink(destination: AnimatedImageDemo()) { Text("Animated Image") }
+                NavigationLink(destination: GeometryReaderDemo()) { Text("Geometry Reader") }
+                NavigationLink(destination: TransitionViewDemo()) { Text("Transition") }
+            }
+            
+            Section(header: Text("Regression Cases")) {
+                NavigationLink(destination: SingleListDemo()) { Text("#1998") }
+            }
         }.navigationBarTitle(Text("SwiftUI Sample"))
     }
 }
