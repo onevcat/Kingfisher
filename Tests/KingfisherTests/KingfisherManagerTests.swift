f832f0afd6f8f0168c2cbf59d05e1c66e8683d15
Roman Podymov <podymfrombryansk@yandex.ru>
2022-10-06 11:07:20 +0200

    Revert

--- Tests/KingfisherTests/KingfisherManagerTests.swift
+++ Tests/KingfisherTests/KingfisherManagerTests.swift
@@ -861,7 +861,7 @@ class KingfisherManagerTests: XCTestCase {
         }
         task?.cancel()
 
-        waitForExpectations(timeout: 3, handler: nil)
+        waitForExpectations(timeout: 1, handler: nil)
     }
 
     func testRetrievingAlternativeSourceCanCancelUpdatedTask() {
