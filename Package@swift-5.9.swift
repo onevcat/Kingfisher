3dea190628331f6d6ad00beaa81b278e2ee9341f
onevcat <onevcat@gmail.com>
2023-06-23 19:36:36 +0900

    Add visionOS as supported platform to SPM

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "Kingfisher",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_14),
+        .tvOS(.v12),
+        .watchOS(.v5),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(name: "Kingfisher", targets: ["Kingfisher"])
+    ],
+    targets: [
+        .target(
+            name: "Kingfisher",
+            path: "Sources"
+        )
+    ]
+)
