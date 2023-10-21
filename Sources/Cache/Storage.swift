ca18a4a688bf6098d0a3db36f9dcecedd5a80007
onevcat <onevcat@gmail.com>
2023-10-21 17:41:32 +0900

    Update doc

--- Sources/Cache/Storage.swift
+++ Sources/Cache/Storage.swift
@@ -102,9 +102,23 @@ public protocol CacheCostCalculable {
     var cacheCost: Int { get }
 }
 
-/// Represents types which can be converted to and from data.
+/// Represents types that can be converted to and from data.
 public protocol DataTransformable {
+    
+    /// Converts the current value to a `Data` representation.
+    /// - Returns: The data object which can represent the value of the conforming type.
+    /// - Throws: If any error happens during the conversion.
     func toData() throws -> Data
+    
+    /// Convert some data to the value.
+    /// - Parameter data: The data object which should represent the conforming value.
+    /// - Returns: The converted value of the conforming type.
+    /// - Throws: If any error happens during the conversion.
     static func fromData(_ data: Data) throws -> Self
+    
+    /// An empty object of `Self`.
+    ///
+    /// > In the cache, when the data is not actually loaded, this value will be returned as a placeholder.
+    /// > This varible should be returned quickly without any heavy operation inside.
     static var empty: Self { get }
 }
