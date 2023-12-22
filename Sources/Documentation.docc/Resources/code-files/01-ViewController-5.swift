07e4fe5ea87a900993876555415863ab6b414389
onevcat <onevcat@gmail.com>
2023-12-22 10:58:06 +0900

    Add tutorial steps

--- Sources/Documentation.docc/Resources/code-files/01-ViewController-5.swift
+++ Sources/Documentation.docc/Resources/code-files/01-ViewController-5.swift
@@ -35,7 +35,7 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath) as! SampleCell
         cell.sampleLabel.text = "Index \(indexPath.row)"
-        cell.sampleImageView.backgroundColor = .gray
+        cell.sampleImageView.backgroundColor = .lightGray
         return cell
     }
 }
