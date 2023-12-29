e4b4c1f9fbf585af798b5f04854ba3418922376a
onevcat <onevcat@gmail.com>
2023-12-29 23:35:39 +0800

    Finish UIKit tutorial

--- Sources/Documentation.docc/Resources/code-files/01-ViewController-6.swift
+++ Sources/Documentation.docc/Resources/code-files/01-ViewController-6.swift
@@ -1,39 +1,3 @@
-//
-//  ViewController.swift
-//  KingfisherSample
-//
-//  Created by Wei Wang on 2023/12/12.
-//
-
-import UIKit
-import Kingfisher
-
-class ViewController: UIViewController {
-
-    lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero)
-        tableView.register(SampleCell.self, forCellReuseIdentifier: "SampleCell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.rowHeight = 80
-        return tableView
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(KingfisherManager.shared)
-        
-        tableView.dataSource = self
-        view.addSubview(tableView)
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
-        ])
-    }
-}
-
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         1
@@ -42,12 +6,12 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath) as! SampleCell
         cell.sampleLabel.text = "Index \(indexPath.row)"
-        cell.sampleImageView.backgroundColor = .lightGray
         
         let urlPrefix = "https://raw.githubusercontent.com/onevcat/Kingfisher-TestImages/master/DemoAppImage/Loading/kingfisher"
         let url = URL(string: "\(urlPrefix)-1.jpg")
         cell.sampleImageView.kf.setImage(with: url)
         
+        cell.sampleImageView.backgroundColor = .lightGray
         return cell
     }
 }
--- Sources/Documentation.docc/Resources/code-files/01-ViewController-8.swift
+++ Sources/Documentation.docc/Resources/code-files/01-ViewController-8.swift
@@ -1,39 +1,3 @@
-//
-//  ViewController.swift
-//  KingfisherSample
-//
-//  Created by Wei Wang on 2023/12/12.
-//
-
-import UIKit
-import Kingfisher
-
-class ViewController: UIViewController {
-
-    lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero)
-        tableView.register(SampleCell.self, forCellReuseIdentifier: "SampleCell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.rowHeight = 80
-        return tableView
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(KingfisherManager.shared)
-        
-        tableView.dataSource = self
-        view.addSubview(tableView)
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
-        ])
-    }
-}
-
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         10
@@ -42,7 +6,6 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath) as! SampleCell
         cell.sampleLabel.text = "Index \(indexPath.row)"
-        cell.sampleImageView.backgroundColor = .clear
         
         let urlPrefix = "https://raw.githubusercontent.com/onevcat/Kingfisher-TestImages/master/DemoAppImage/Loading/kingfisher"
         let url = URL(string: "\(urlPrefix)-\(indexPath.row + 1).jpg")
@@ -51,7 +14,7 @@ extension ViewController: UITableViewDataSource {
         
         let roundCorner = RoundCornerImageProcessor(radius: .widthFraction(0.5), roundingCorners: [.topLeft, .bottomRight])
         cell.sampleImageView.kf.setImage(with: url, options: [.processor(roundCorner)])
-        
+        cell.sampleImageView.backgroundColor = .clear
         return cell
     }
 }
