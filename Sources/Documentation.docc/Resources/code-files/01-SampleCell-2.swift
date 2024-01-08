46789ab056937fd7fdb0912d4e341c3bed95ffe6
onevcat <onevcat@gmail.com>
2024-01-08 14:45:32 +0900

    Finish UIKit tutorial

--- Sources/Documentation.docc/Resources/code-files/01-SampleCell-2.swift
+++ Sources/Documentation.docc/Resources/code-files/01-SampleCell-2.swift
@@ -1,12 +1,4 @@
-//
-//  SampleCell.swift
-//  KingfisherSample
-//
-//  Created by Wei Wang on 2023/12/12.
-//
-
 import UIKit
-import Kingfisher
 
 class SampleCell: UITableViewCell {
     var sampleImageView: UIImageView = {
--- Sources/Documentation.docc/Resources/code-files/01-ViewController-4.swift
+++ Sources/Documentation.docc/Resources/code-files/01-ViewController-4.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  KingfisherSample
-//
-//  Created by Wei Wang on 2023/12/12.
-//
-
 import UIKit
 import Kingfisher
 
