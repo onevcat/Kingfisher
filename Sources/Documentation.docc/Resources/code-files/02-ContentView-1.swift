505d74f19aa2e209ccbb7594bd84cc3c355eed37
onevcat <onevcat@gmail.com>
2024-01-08 15:37:59 +0900

    Start writing SwiftUI tutorial

---
+++ Sources/Documentation.docc/Resources/code-files/02-ContentView-1.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ Sources/Documentation.docc/Resources/code-files/02-ContentView-2.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+import Kingfisher
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear {
+            print(KingfisherManager.shared)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
