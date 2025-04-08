fc1d2c69c85d9023ae2a0038130dd0a5458c1ee4
Erinç Olkan Dokumacıoğlu <erincolkan@hotmail.com>
2025-04-08 12:05:04 +0300

    Fix ineffective configuration parameter in DiskStorage
    invalidate previous timer and start a new one upon new configuration.

--- Sources/Cache/MemoryStorage.swift
+++ Sources/Cache/MemoryStorage.swift
@@ -75,6 +75,11 @@ public enum MemoryStorage {
             didSet {
                 storage.totalCostLimit = config.totalCostLimit
                 storage.countLimit = config.countLimit
+                cleanTimer?.invalidate()
+                cleanTimer = .scheduledTimer(withTimeInterval: config.cleanInterval, repeats: true) { [weak self] _ in
+                    guard let self = self else { return }
+                    self.removeExpired()
+                }
             }
         }
 
